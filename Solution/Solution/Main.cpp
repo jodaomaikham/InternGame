a25ee88a60ec1e0b737ffcddce592bf3fad2e4b6
JO <88376410+jodaomaikham@users.noreply.github.com>
2021-08-10 00:33:18 +0700

    Update Main.cpp

--- Solution/Solution/Main.cpp
+++ Solution/Solution/Main.cpp
@@ -1,42 +1,35 @@
 #include <stdio.h>
+#include <iostream>
 
-void swap(int& num1, int& num2)
+using namespace std;
+//Referent
+void swap1(int& num1, int& num2)
 {
 	int temp;
 	temp = num1;
 	num1 = num2;
 	num2 = temp;
 }
-void swap(int*, int*);
-void swap(int* num1, int* num2)
+//Pointers
+void swap2(int* num1, int* num2)
 {
-	int t = *num1;
+	int temp = *num1;
 	*num1 = *num2;
-	*num2 = t;
+	*num2 = temp;
 }
+
 int main()
 {
-	//Swap by Referent.
-	printf("Swap function by Referent\n");
-	int a;
-	int b;
-
-	printf("Enter first number : \n");
-	scanf_s("%d", &a);
-	printf("Enter second number : \n");
-	scanf_s("%d", &b);
-	swap(a, b);
-	printf("after swap: \n%d \n%d\n", a, b);
-
-	//swap by Referent using pointers.
-	printf(" \nSwap by Referent using pointers\n");
-	int x;
-	int y;
-	printf("Enter first number :\n");
-	scanf_s("%d", &x);
-	printf("Enter second number : \n");
-	scanf_s("%d", &y);
-	swap(&x, &y);
-	printf("Result swap : \n%d\n%d\n", x, y);
+	int a, b;
+	cout << "Enter number to swap" << endl;
+	swap1(a,b);
+	cin >> a >> b;
+	cout << "Before swap" << endl;
+	cout << "a :" << a << endl;
+	cout << "b :" << b << endl;
+	swap2(&a, &b);
+	cout << "After swap" << endl;
+	cout << "a : " << a << endl;
+	cout << "b : " << b << endl;
 	return 0;
 }
